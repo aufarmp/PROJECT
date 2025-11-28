3ee19d14f05231b543a881a30235fb5ea0912f21
aufarmp <mpaufar@gmail.com>
2025-11-28 16:08:00 +0700

    new-login-caching-update

--- comic-web/code/login.php
+++ comic-web/code/login.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-include 'connection.php'; 
 
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
+
+include 'connection.php'; 
 
 if (isset($_SESSION['user_id'])) {
     if ($_SESSION['role'] == 'admin') {
